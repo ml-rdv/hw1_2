4a461468179989a9c51bb181ae33e40463f0b108
Liudmyla Radova <ludmila.radova2418@gmail.com>
2023-07-26 16:37:32 +0300

    Commit

--- src/hw1_1.java
+++ src/hw1_1.java
@@ -1,3 +1,4 @@
+// Отсортировать массив от меньшего к большему придумав свой алгоритм сортировки.
 public class hw1_1 {
     public static void main(String[] args) {
         int[] arr = new int[]{2, 524, 3, 1, 9, -1, 2345};
---
+++ src/hw1_2.java
@@ -0,0 +1,20 @@
+// Отсортировать массив пузырьковым алгоритмом.
+// Сложность лучшего, худшего и среднего случая = n^2
+public class hw1_2 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 524, 3, 1, 9, -1, 2345};
+        for (int j = 0; j < arr.length; j++) {
+            for (int i = 0; i < arr.length - (j + 1); i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int k = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = k;
+                }
+            }
+        }
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
