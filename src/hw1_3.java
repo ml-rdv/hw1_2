fe9790bf23f07f2c6af5894abde178d8ce4d81f4
Liudmyla Radova <ludmila.radova2418@gmail.com>
2023-07-27 19:51:07 +0300

    Commit

--- src/hw1_3.java
+++ src/hw1_3.java
@@ -1,2 +1,20 @@
+// Отсортировать массив вставками.
 public class hw1_3 {
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 524, 3, 1, 9, -1, 2345, 2, 524, 3, 1, 9, -1, 2345};
+        for (int i = 0; i < arr.length; i++) {
+            int x = arr[i];
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j] <= x) {
+                    break;
+                }
+                int k = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = k;
+            }
+        }
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
 }
