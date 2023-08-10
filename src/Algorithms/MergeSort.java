2333dbcc4456540980f801628c30ceca194e3019
Liudmyla Radova <ludmila.radova2418@gmail.com>
2023-08-10 13:09:32 +0300

    Реализация сортировки слиянием

--- src/Algorithms/MergeSort.java
+++ src/Algorithms/MergeSort.java
@@ -1,10 +1,57 @@
 package Algorithms;
 // Отсортировать массив слиянием.
-// Сложность лучшего = , худшего = , среднего случая =
+// Сложность лучшего, худшего, среднего случая = n * log(n)
+
+
+import java.util.Arrays;
 
 public class MergeSort {
     public static void main(String[] args) {
-        int[] arr = new int[]{2, 4, 524, 3, 1, 9, -1, 2345};
+        int[] arr = new int[]{2, 4, 524, 3};
+        arr = sortArray(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 
+    public static int[] sortArray(int[] array) {
+        if (array == null) {
+            return null;
+        }
+        if (array.length < 2) {
+            return array;
+        }
+
+        int[] arrayB = new int[array.length / 2];
+        System.arraycopy(array, 0, arrayB, 0, array.length / 2);
+
+        int[] arrayC = new int[array.length - arrayB.length];
+        System.arraycopy(array, arrayB.length, arrayC, 0, array.length - arrayB.length);
+
+        sortArray(arrayB);
+        sortArray(arrayC);
+
+        mergeArray(array, arrayB, arrayC);
+
+        return array;
+    }
+
+    private static void mergeArray(int[] array, int[] arrayB, int[] arrayC) {
+        int positionB = 0;
+        int positionC = 0;
+
+        for (int c = 0; c < array.length; c++) {
+            if (positionB == arrayB.length) {
+                array[c] = arrayC[positionC];
+                positionC++;
+            } else if (positionC == arrayC.length) {
+                array[c] = arrayB[positionB];
+                positionB++;
+            } else if (arrayB[positionB] < arrayC[positionC]) {
+                array[c] = arrayB[positionB];
+                positionB++;
+            } else {
+                array[c] = arrayC[positionC];
+                positionC++;
+            }
+        }
     }
 }
