abbcaf2ef355c9d9f9feb088d60844b26b364697
ml-rdv <ludmila.radova2418@gmail.com>
2023-11-16 16:49:01 +0200

    Добавлены equals, hashCode, toString в класс Check + Тесты с объектами класса Check

--- src/InternetCafe/Check.java
+++ src/InternetCafe/Check.java
@@ -1,6 +1,7 @@
 package InternetCafe;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class Check {
     private int number;
@@ -14,7 +15,35 @@ public class Check {
         this.price = order.getPrice();
     }
 
+    public Check(int orderId, Date orderDate, double price) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+        this.price = price;
+    }
+
     public void printCheck() {
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Check check = (Check) o;
+        return number == check.number && orderId == check.orderId && Double.compare(price, check.price) == 0 && Objects.equals(orderDate, check.orderDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number, orderId, orderDate, price);
+    }
+
+    @Override
+    public String toString() {
+        return "Check{" +
+                "number=" + number +
+                ", orderId=" + orderId +
+                ", orderDate=" + orderDate +
+                ", price=" + price +
+                '}';
+    }
 }
--- src/PracticeJavaMemoryModelLinksObject/Task4Test.java
+++ src/PracticeJavaMemoryModelLinksObject/Task4Test.java
@@ -1,23 +1,34 @@
 package PracticeJavaMemoryModelLinksObject;
 
+import InternetCafe.Check;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Date;
 
 public class Task4Test {
+
+    // Сравнение с помощью "=="
     @Test
     public void should_compare_objects_incorrectly() {
-        Task4 check1 = new Task4(1, Date.valueOf("1988-09-29"), 200.3);
-        Task4 check2 = new Task4(1, Date.valueOf("1988-09-29"), 200.3);
+        Check check1 = new Check(1, Date.valueOf("1988-09-29"), 200.3);
+        Check check2 = new Check(1, Date.valueOf("1988-09-29"), 200.3);
         Assertions.assertNotSame(check1, check2);
     }
 
+    // Сравнение с помощью equals
     @Test
     public void should_compare_objects_by_equals() {
-        Task4 check1 = new Task4(1, Date.valueOf("1988-09-29"), 200.3);
-        Task4 check2 = new Task4(1, Date.valueOf("1988-09-29"), 200.3);
+        Check check1 = new Check(1, Date.valueOf("1988-09-29"), 200.3);
+        Check check2 = new Check(1, Date.valueOf("1988-09-29"), 200.3);
         Assertions.assertEquals(check1, check2);
     }
 
+    // Сравнение с помощью equals
+    @Test
+    public void should_return_false_compare_objects_by_equals() {
+        Check check1 = new Check(1, Date.valueOf("1979-09-29"), 200.3);
+        Check check2 = new Check(1, Date.valueOf("1988-09-29"), 200.3);
+        Assertions.assertNotEquals(check1, check2);
+    }
 }
