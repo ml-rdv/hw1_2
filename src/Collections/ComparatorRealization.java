27a5bf90837a551fa6dcecf7bbc4eca4995348fa
ml-rdv <ludmila.radova2418@gmail.com>
2024-01-22 20:02:21 +0100

    Создать свой класс Comparator для класса из задачи по ООП и также написать тест на сортировку В список нужно добавить null’ы и сделать так, чтобы сортировка могла их  обработать

--- src/Collections/ComparatorRealization.java
+++ src/Collections/ComparatorRealization.java
@@ -9,15 +9,25 @@ import java.util.Comparator;
  * В список нужно добавить null’ы и сделать так, чтобы сортировка могла их  обработать
  */
 
-// ДОРАБОТАТЬ
-// ПРОБЛЕМА С NULL
-
 public class ComparatorRealization implements Comparator<Client> {
 
-    public int compare(Client a, Client b){
+    public int compare(Client a, Client b) {
 
-        String fullName1 = a == null ? null : a.getFullName();
-        String fullName2 = b == null ? null : b.getFullName();
+        String fullName1 = a == null ? "" : a.getFullName();
+        String fullName2 = b == null ? "" : b.getFullName();
+
+        // Вариант 2
+        /*
+        if (a == null) {
+            return Integer.MIN_VALUE;
+        }
+        if (b == null) {
+            return Integer.MAX_VALUE;
+        }
+
+        String fullName1 = a.getFullName();
+        String fullName2 = b.getFullName();
+        */
 
         return fullName1.compareTo(fullName2);
     }
--- src/Collections/ComparatorRealizationTest.java
+++ src/Collections/ComparatorRealizationTest.java
@@ -8,23 +8,20 @@ import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
-// ДОРАБОТАТЬ
-// ПРОБЛЕМА С  NULL
-
 public class ComparatorRealizationTest {
 
-    // ДОРАБОТАТЬ
     // Сортировка по fullName
     @Test
     void should_sort_using_Comparator() {
         final ComparatorRealization comparator = new ComparatorRealization();
         List<Client> clients = new ArrayList<>();
         clients.add(new Client("Ivan", 1, "email", Date.valueOf("1997-03-10")));
-        clients.add(new Client("Maria", 1, "email", Date.valueOf("1997-03-10")));
-        clients.add(new Client("Egor", 1, "email", Date.valueOf("1997-03-10")));
-        clients.add(new Client("Anastasia", 1, "email", Date.valueOf("1997-03-10")));
-        // ДОРАБОТАТЬ
         clients.add(null);
+        clients.add(new Client("Maria", 1, "email", Date.valueOf("1997-03-10")));
+        clients.add(null);
+        clients.add(new Client("Egor", 1, "email", Date.valueOf("1997-03-10")));
+        clients.add(null);
+        clients.add(new Client("Anastasia", 1, "email", Date.valueOf("1997-03-10")));
         clients.sort(comparator);
         System.out.println(clients);
     }
