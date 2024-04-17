c0ef0b86045e70f5e232cadbee0064c09dd2258a
ml-rdv <ludmila.radova2418@gmail.com>
2024-04-17 17:31:00 +0300

    Не понимаю, почему "current thread is not owner"?

--- src/Multithreading/CashRegister/CashRegister.java
+++ src/Multithreading/CashRegister/CashRegister.java
@@ -1,5 +1,7 @@
 package Multithreading.CashRegister;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 /**
  * Реализовать класс касса. Касса может обслуживать только 1 покупателя, когда покупатель подходит к кассе,
  * то он её занимает на какое-то время (1 сек) и затем освобождает для другого покупателя.
@@ -10,73 +12,48 @@ package Multithreading.CashRegister;
  */
 
 public class CashRegister {
-    static CashRegister monitor = new CashRegister();
+    static CashRegister buyer = new CashRegister();
+    static final CashRegister monitor1 = new CashRegister();
+    static final CashRegister monitor2 = new CashRegister();
+    static AtomicBoolean isCashRegisterBusy = new AtomicBoolean(false);
 
     public static void main(String[] args) {
-        new Client1().start();
-        new Client2().start();
-        new Client3().start();
+        for (int i = 1; i < 6; i++) {
+            int finalI = i;
+            // Создание покупателей в цикле
+            new Thread(() -> {
+                try {
+                    buyer.payUsingWait(finalI);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }).start();
+        }
     }
 
-    public static synchronized void payUsingSleep(int number) {
-        try {
+    public void payUsingWait(int number) throws InterruptedException {
+        if (isCashRegisterBusy.get()) {
+            waiting(number);
+        } else {
+            paying(number);
+        }
+    }
+
+    private void waiting(int number) throws InterruptedException {
+        synchronized (monitor1) {
+            System.out.printf("Касса занята, покупатель %d ожидает\n", number);
+            wait();
+        }
+    }
+
+    private void paying(int number) throws InterruptedException {
+        synchronized (monitor2) {
+            isCashRegisterBusy.getAndSet(true);
             System.out.printf("Покупатель %d занял кассу\n", number);
             Thread.sleep(1000);
             System.out.printf("Покупатель %d освободил кассу\n", number);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    /*
-    Не понимаю, как реализовать через wait/notify
-    wait усыпляет поток, предоставляя доступ другому потоку, но как это поможет
-    в данном случае решить задачу?
-
-    Это не работающий набросок
-
-    public synchronized void payUsingWait(int number) {
-        try {
-            System.out.printf("Касса занята, покупатель %d ожидает\n", number);
-            wait();
-            System.out.printf("Покупатель %d занял кассу\n", number);
-            Thread.sleep(1000);
-        }
-        catch (InterruptedException e) {
-        }
-        System.out.printf("Покупатель %d освободил кассу\n", number);
-        notify();
-    }
-    */
-}
-
-class Client1 extends Thread {
-    @Override
-    public void run() {
-        for (int i = 0; i < 5; i++) {
-//            CashRegister.monitor.payUsingWait(1);
-            CashRegister.payUsingSleep(1);
-        }
-    }
-}
-
-class Client2 extends Thread {
-    @Override
-    public void run() {
-        for (int i = 0; i < 5; i++) {
-//            CashRegister.monitor.payUsingWait(2);
-            CashRegister.payUsingSleep(2);
-        }
-    }
-}
-
-class Client3 extends Thread {
-    @Override
-    public void run() {
-        for (int i = 0; i < 5; i++) {
-//            CashRegister.monitor.payUsingWait(3);
-            CashRegister.payUsingSleep(3);
+            isCashRegisterBusy.getAndSet(false);
+            notifyAll();
         }
     }
 }
