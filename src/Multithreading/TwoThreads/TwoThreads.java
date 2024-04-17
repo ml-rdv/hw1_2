02705a3acc203b1a15efa12afe4ee6698ce61813
ml-rdv <ludmila.radova2418@gmail.com>
2024-04-17 16:21:50 +0300

    Корректировки по задаче

--- src/Multithreading/TwoThreads/TwoThreads.java
+++ src/Multithreading/TwoThreads/TwoThreads.java
@@ -1,7 +1,5 @@
 package Multithreading.TwoThreads;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 /**
  * Реализовать 2 потока через класс Thread.
  * a. Первый поток в цикле каждую 1 мс выводит целочисленное поле count в консоль. Условие выхода
@@ -12,58 +10,37 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 
 public class TwoThreads {
-    public static void main(String[] args) {
-        FirstThread firstThread = new FirstThread();
-        SecondThread secondThread = new SecondThread();
-
-//        Если устанавливаю флаг демон, потоки останавливают работу
-//        вместе с программой
-//        firstThread.setDaemon(true);
-//        secondThread.setDaemon(true);
+    static int count = 0;
+    public static void main(String[] args) throws InterruptedException {
+        Thread firstThread = new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                System.out.println(count);
+                try {
+                    Thread.currentThread().sleep(1);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                    break;
+                }
+            }
+        });
+        Thread secondThread = new Thread(() -> {
+            int n = 1000;
+            while (count != n) {
+                count++;
+                try {
+                    Thread.currentThread().sleep(1);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
 
         firstThread.start();
         secondThread.start();
 
-//        Чтобы потоки успели вывести хоть что-то
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException ex) {
-            ex.printStackTrace();
-        }
+        // ждём завершения второго потока
+        secondThread.join();
 
         firstThread.interrupt();
-        secondThread.interrupt();
-    }
-}
-
-class FirstThread extends Thread {
-    @Override
-    public void run() {
-        while (!isInterrupted()) {
-            System.out.println(SecondThread.count.get());
-            try {
-                FirstThread.sleep(1);
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-                break;
-            }
-        }
-    }
-}
-
-class SecondThread extends Thread {
-    protected final static AtomicInteger count = new AtomicInteger();
-
-    @Override
-    public void run() {
-        int n = 1000;
-        while (count.get() != n) {
-            count.incrementAndGet();
-            try {
-                SecondThread.sleep(1);
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-            }
-        }
     }
 }
